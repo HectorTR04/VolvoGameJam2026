f56bdb53186c70757a376112a0ce237d76f1585f
HecticSwede <hectorastr@gmail.com>
2026-02-28 00:05:23 +0100

    Input Actions Update + Player Animation
    - Created PlayerAnimator state machine to handle changing between animations
    - Able to transition from idle to walking
    - Added Interact action in Input Actions Asset
    - Transition between idle and interact works well
    - Transition from walking to interact not fully working, though might not be necessary

---
+++ Assets/Scripts/Player/Animation/IdleState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class IdleState : AnimationState
+{
+    public IdleState(Animator playerAnimator)
+    {
+        m_playerAnimator = playerAnimator;
+    }
+    public override void Enter()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator m_animator;
+    private AnimationState currentState;
+    private bool m_isInteracting;
+
+    public AnimationState CurrentState { get { return currentState; } }
+
+    #region Unity Methods
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        currentState = new IdleState(m_animator);
+        currentState.Enter();
+    }
+    #endregion
+
+    public void Animate(Vector2 movement)
+    {
+        if (currentState is InteractState)
+        {
+            AnimatorStateInfo stateInfo = m_animator.GetCurrentAnimatorStateInfo(0);
+
+            if (stateInfo.normalizedTime >= 1f)
+            {
+                m_isInteracting = false;
+            }
+        }
+        if (movement != Vector2.zero && currentState is not WalkState && !m_isInteracting)
+        {
+            TransitionToNewState(new WalkState(m_animator));
+        }
+        if(movement == Vector2.zero && currentState is not IdleState && !m_isInteracting)
+        {
+            TransitionToNewState(new IdleState(m_animator));
+        }
+    }
+
+    public void AnimateInteraction()
+    {
+        Debug.Log("interacted");
+        m_isInteracting = true;
+        TransitionToNewState(new InteractState(m_animator));
+    }
+    private void TransitionToNewState(AnimationState nextState)
+    {
+        currentState.Exit();
+
+        currentState = nextState;
+        currentState.Enter();
+    }
+}
