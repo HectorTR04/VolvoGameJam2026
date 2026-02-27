0362a664cdd315ffabe34a607876a9557b7a37b9
Sakyey <gustaf.m.dahlqvist@gmail.com>
2026-02-27 21:30:21 +0100

    nice change

--- Assets/Scripts/IncineratorScript.cs
+++ Assets/Scripts/IncineratorScript.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class IncineratorScript : MonoBehaviour
 {
+    [SerializeField]
+    private ParticleSystem incineratorParticles;
 
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            incineratorParticles.Play();
+        }
+    }
 }
