ccd3f739daf55a4be947548ea427b9710e01103e
HecticSwede <hectorastr@gmail.com>
2026-03-01 00:23:04 +0100

    Added item interaction
    - Can now swap, drop and pickup items
    - OnInteract is running correctly, can be added to other interactions

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Item : MonoBehaviour, IInteractable
 {
     BaseItem baseData;
 
     public Item(BaseItem baseData)
     {
         this.baseData = baseData;
+    }
+
+    public void OnInteract()
+    {
+
     }
 
     void Start()
--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -4,25 +4,70 @@ public class PlayerInteraction : MonoBehaviour
 {
     private Vector3 m_verticalOffset = new(0, 0.5f, 0);
     private GameObject m_heldItem;
+    private readonly Vector3 m_aboveHeadOffset = new(0, 2f, 0);
     private readonly float m_interactionRange = 2f;
-
-    #region Unity Methods
-    private void Update()
-    {
-        
-    }
-    #endregion
 
     public void Interact()
     {
         if (Physics.Raycast(transform.position + m_verticalOffset, transform.forward, out RaycastHit hit, m_interactionRange))
         {
             Debug.DrawRay(transform.position + m_verticalOffset, transform.forward * hit.distance, Color.red);
-            if(hit.collider == null ) { return; }
-            if(hit.collider.gameObject.TryGetComponent(out IInteractable detectedInteraction))
+            if (hit.collider.gameObject.TryGetComponent(out IInteractable detectedInteraction))
             {
+                Debug.Log("Interaction Detected With: " + hit.collider.gameObject);
                 detectedInteraction.OnInteract();
             }
+            if (hit.collider.gameObject.TryGetComponent(out Item detectedItem))
+            {
+                Debug.Log("Item Detected: " + hit.collider.gameObject);
+                HandleItemInteraction(detectedItem.gameObject);
+                return;
+            }        
         }
+        if (m_heldItem != null)
+        {
+            DropHeldItem();
+        }
+    }
+
+    private void HandleItemInteraction(GameObject detectedItem)
+    {
+        if(m_heldItem == null)
+        {
+            PickUpItem(detectedItem);
+        }
+        else
+        {
+            SwapHeldItem(detectedItem);
+        }
+    }
+
+    private void DropHeldItem()
+    {
+        m_heldItem.transform.parent = null;
+        m_heldItem.GetComponent<Collider>().enabled = true;
+        m_heldItem.transform.position = transform.position + transform.forward * 1.5f;
+        Debug.Log("Item: " + m_heldItem + "dropped.");
+        m_heldItem = null;
+    }
+
+    private void PickUpItem(GameObject detectedItem)
+    {
+        m_heldItem = detectedItem;
+        m_heldItem.transform.SetParent(gameObject.transform);
+        m_heldItem.transform.position = gameObject.transform.position + m_aboveHeadOffset;
+        m_heldItem.GetComponent<Collider>().enabled = false;
+    }
+
+    private void SwapHeldItem(GameObject detectedItem)
+    {
+        m_heldItem.transform.parent = null;
+        m_heldItem.GetComponent<Collider>().enabled = true;
+        m_heldItem.transform.position = transform.position + transform.forward * 1.5f;
+        Debug.Log("Swapped Item: " + m_heldItem + "for: " + detectedItem);
+        m_heldItem = detectedItem;
+        m_heldItem.transform.SetParent(gameObject.transform);
+        m_heldItem.transform.position = gameObject.transform.position + m_aboveHeadOffset;
+        m_heldItem.GetComponent<Collider>().enabled = false;
     }
 }
