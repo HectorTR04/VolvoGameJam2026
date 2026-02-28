61527e7fe9e6384f55e218259b01612a5005b496
Sakyey <gustaf.m.dahlqvist@gmail.com>
2026-02-28 15:23:46 +0100

    Added machine base and "finished" energymanager
    + Machine base class is an abstract class which all machines will inherit from.
    + Energymanager is "finished"
    * Added methods for Turning on and off, registering and unregistering machines etc. Forced turnoff.

--- Assets/Scripts/Machine/IncineratorScript.cs
+++ Assets/Scripts/Machine/IncineratorScript.cs
@@ -2,58 +2,39 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class IncineratorScript : MonoBehaviour
+public class IncineratorScript : MachineBase
 {
-    [SerializeField]
-    private ParticleSystem incineratorParticles;
-    [SerializeField]
-    private EnergyManager energyManager;
-    [SerializeField]
-    private float drainPerSecond = 1f;
-    [SerializeField]
-    private float energyPerTrash = 10f;
-    private bool isOn;
+    [SerializeField] private ParticleSystem incineratorParticles;
+    [SerializeField] private float energyPerTrash = 10f;
     void Update()
     {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             if (isOn)
             {
-                TurnOffIncinerator();
+                TurnOff();
             }
             else
             {
-                TurnOnIncinerator();
+                TurnOn();
             }
         }
-
-        if (!isOn || energyManager == null)
-        {
-            return;
-        }
-        energyManager.energyLevel -= Time.deltaTime * drainPerSecond;
-        if (energyManager.energyLevel <= 0)
-        {
-            energyManager.energyLevel = 0f;
-            TurnOffIncinerator();
-        }
-        Debug.Log($"Energy Level: {energyManager.energyLevel}");
     }
-    public void TurnOnIncinerator()
-    {
-        if (isOn || energyManager == null) return;
-        if (energyManager.energyLevel <= 0f) return; // optional: don't allow turning on at 0
+    // public void TurnOnIncinerator()
+    // {
+    //     if (isOn || energyManager == null) return;
+    //     if (energyManager.energyLevel <= 0f) return; // optional: don't allow turning on at 0
 
-        if (incineratorParticles) incineratorParticles.Play();
-        isOn = true;
-    }
-    public void TurnOffIncinerator()
-    {
-        if (!isOn) return;
+    //     if (incineratorParticles) incineratorParticles.Play();
+    //     isOn = true;
+    // }
+    // public void TurnOffIncinerator()
+    // {
+    //     if (!isOn) return;
 
-        if (incineratorParticles) incineratorParticles.Stop();
-        isOn = false;
-    }
+    //     if (incineratorParticles) incineratorParticles.Stop();
+    //     isOn = false;
+    // }
     public void OnTriggerEnter(Collider other)
     {
         if (!isOn || energyManager == null) return;
@@ -61,8 +42,15 @@ public class IncineratorScript : MonoBehaviour
         if (other.CompareTag("Trash"))
         {
             other.gameObject.SetActive(false);
-            energyManager.energyLevel += energyPerTrash;
+            energyManager.AddEnergy(energyPerTrash);
         }
     }
-
+    protected override void OnTurnedOn()
+    {
+        if(incineratorParticles) incineratorParticles.Play();
+    }
+    protected override void OnTurnedOff()
+    {
+        if(incineratorParticles) incineratorParticles.Stop();
+    }
 }
--- Assets/Scripts/Machine/MachineBase.cs
+++ Assets/Scripts/Machine/MachineBase.cs
@@ -1,6 +1,61 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class MachineBase : MonoBehaviour
 {
     [SerializeField] protected EnergyManager energyManager;
+    [SerializeField] private float drainPerSecond = 1f;
+
+    public float DrainPerSecond => isOn ? drainPerSecond : 0f;
+    protected bool isOn;
+
+    protected virtual void Awake()
+    {
+        if(energyManager == null)
+        {
+            energyManager = FindAnyObjectByType<EnergyManager>();
+        }
+    }
+    private void Update()
+    {
+        
+    }
+    public void TurnOn()
+    {
+        if(isOn) return;
+        if(energyManager == null) return;
+        if (!energyManager.HasEnergy(0.01f)) //If no energy, don't turn on
+        {
+            return;
+        }
+        isOn = true;
+        energyManager.Register(this);
+        OnTurnedOn();
+    }
+    public void TurnOff()
+    {
+        if(!isOn) return;
+        if (energyManager != null)
+        {    
+            energyManager.Unregister(this);
+        }
+        isOn = false;
+        OnTurnedOff();
+    }
+    public void ForcedOffDueToNoEnergy()
+    {
+        if(!isOn) return;
+        isOn = false;
+        OnPowerLost();
+    }
+    protected abstract void OnTurnedOn();
+    protected abstract void OnTurnedOff();
+    protected virtual void OnPowerLost() => OnTurnedOff();
+    protected virtual void OnDisable()
+    {
+        if(isOn && energyManager != null)
+        {
+            energyManager.Unregister(this);
+        }
+    }
 }
