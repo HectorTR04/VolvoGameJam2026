ca36e3df21df04f00536a1c490d8dd52ef9dabbc
Sakyey <gustaf.m.dahlqvist@gmail.com>
2026-03-02 14:57:15 +0100

    Suggestionbox

--- Assets/Scripts/SuggestionBox.cs
+++ Assets/Scripts/SuggestionBox.cs
@@ -4,7 +4,11 @@ public class SuggestionBox : MonoBehaviour {}
 
 // ----------- This is the script where we fucking lock in and shape up --------------
 /*
-    Recipe book    - List of all recipes (or at least hints) (also clipboard, we just toggle which "page" to turn to)
-    Explain enery and emissions to the player (in the clipboard)
-
+    1. Recipe book    - List of all recipes (or at least hints) (also clipboard, we just toggle which "page" to turn to)
+    2. Explain enery and emissions to the player (in the clipboard)
+    3. A page which explains the different machines and their functions (also in the clipboard)
+    4. Get a clear directory in what we want the game to be. What type of game do we want to make? 
+    5. Clearer progression in what can be made with what materials.
+    6. Upgrades and more machines. (Shop as well)
+    7. 
 */
