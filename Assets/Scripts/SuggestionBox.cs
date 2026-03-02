808c14689a0d597e64fbdd1171e0c155801ca4e7
Sakyey <gustaf.m.dahlqvist@gmail.com>
2026-03-02 14:47:09 +0100

    shitty ah

---
+++ Assets/Scripts/SuggestionBox.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SuggestionBox : MonoBehaviour {}
+
+// ----------- This is the script where we fucking lock in and shape up --------------
+/*
+    Recipe book    - List of all recipes (or at least hints) (also clipboard, we just toggle which "page" to turn to)
+    Explain enery and emissions to the player (in the clipboard)
+
+*/
